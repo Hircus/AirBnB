b8865640152dd9806e2869ef4bf0c6f8d2d0c39e
Senghor <senghormboukap1@gmail.com>
2021-06-24 11:35:19 +0200

    Prenotazione

--- Progetto/src/Prenotazione.java
+++ Progetto/src/Prenotazione.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Prenotazione {
+public class Prenotazione {
+    private int id;
+    private int dataInizio;
+    private int dataFine;
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDataInizio(int dataInizio) {
+        this.dataInizio = dataInizio;
+    }
+
+    public void setDataFine(int dataFine) {
+        this.dataFine = dataFine;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDataInizio() {
+        return dataInizio;
+    }
+
+    public int getDataFine() {
+        return dataFine;
+    }
 }
