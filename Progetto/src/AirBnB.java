f00ae42ac6dcf3a93774ad05af3ff9ddbf703607
Antonio <a.cossu35@studenti.unica.it>
2021-06-24 12:48:44 +0200

    small change

--- Progetto/src/AirBnB.java
+++ Progetto/src/AirBnB.java
@@ -1,2 +1,13 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeSet;
+
 public class AirBnB {
+    HashMap<UtenteHost, HashSet<Abitazione>> host_setAbitazioni;
+    HashMap<Utente, TreeSet<Prenotazione>> utente_setPrenotazioni;
+    HashMap<Abitazione, TreeSet<Prenotazione>> abitazione_setPrenotazioni;
+
+    public static void main(String[] args) {
+        AirBnB a = new AirBnB();
+    }
 }
