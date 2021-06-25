33564a59a7debd792a61ae1309d012bad58d8606
Senghor <senghormboukap1@gmail.com>
2021-06-25 09:23:36 +0200

    Merge branch 'main' of C:\Users\Enterprise\Desktop\AirBnB with conflicts.

--- Progetto/src/Prenotazione.java
+++ Progetto/src/Prenotazione.java
@@ -2,16 +2,19 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public class Prenotazione {
+public Prenotazione (UUID id, LocalDate dataInizio, LocalDate dataFine){
+
+    this.id = id;
+    this.dataInizio = dataInizio;
+    this.dataFine = dataFine;
+
+}
+
 
     private UUID id;
     private LocalDate dataInizio;
     private LocalDate dataFine;
 
-    public Prenotazione (LocalDate dataInizio, LocalDate dataFine){
-        this.id = UUID.randomUUID();
-        this.dataInizio = dataInizio;
-        this.dataFine = dataFine;
-    }
 
     public void setId(UUID id) {
         this.id = id;
@@ -37,6 +40,7 @@ public class Prenotazione {
         return dataFine;
     }
 
-    public void addFeedback( Prenotazione p){
-    }
+public void addFeedback( Prenotazione p){
+}
+
 }
