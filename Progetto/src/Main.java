7568ff898cdc1829704fc8e501de074d893cfb9a
oxrock <pierluigi.filosa@gmail.com>
2021-06-25 14:58:58 +0200

    update Main

--- Progetto/src/Main.java
+++ Progetto/src/Main.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.Set;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) throws UtenteNotFoundException {
@@ -13,7 +14,7 @@ public class Main {
         LocalDate data2= LocalDate.now().plusMonths(1);
 
         Utente antonio = new Utente("Antonio", "Cossu", "cossu@mail.com");
-        Utente hemilian = new Utente("Hemilian", "Hrisca", "hrisca@mail.com");
+        Utente emilian = new Utente("Hemilian", "Hrisca", "hrisca@mail.com");
         Utente pierluigi = new Utente("Pierluigi", "Filosa", "filosa@mail.com");
         Utente senghor = new Utente("Senghor", "Njampo", "njampo@mail.com");
         Utente senghorlavendetta = new Utente("Senghor", "Njampo", "njampo@mail.com");
@@ -28,7 +29,6 @@ public class Main {
         Prenotazione p6 = new Prenotazione(LocalDate.now(), LocalDate.now().plusDays(3));
         Prenotazione p7 = new Prenotazione(LocalDate.now().minusDays(3), LocalDate.now().plusDays(9));
 
-
         Abitazione a1 = new Abitazione(30, "Casa sul mare", "Via Ceppa 1", 3, 2, 4, data1, data2);
         Abitazione a2 = new Abitazione(50, "Casetta in montagna", "Via Bla 1", 2, 1, 4, data1, data2);
         Abitazione a3 = new Abitazione(70, "B&B in città", "Via Poo 1", 5, 1, 4, data1, data2);
@@ -37,7 +37,7 @@ public class Main {
         a.addUtente(senghorlavendetta);
         a.addUtente(pierluigi);
         a.addUtente(antonio);
-        a.addUtente(hemilian);
+        a.addUtente(emilian);
         a.addUtente(melvin);
         a.addUtente(riccardo);
 
@@ -46,8 +46,8 @@ public class Main {
         a.addHostAbitazioni(melvin.getId(), a3);
 
         a.addUtentePrenotazione(antonio.getId(), p1);
-        a.addUtentePrenotazione(hemilian.getId(), p2);
-        a.addUtentePrenotazione(hemilian.getId(), p3);
+        a.addUtentePrenotazione(emilian.getId(), p2);
+        a.addUtentePrenotazione(emilian.getId(), p3);
         a.addUtentePrenotazione(pierluigi.getId(), p4);
         a.addUtentePrenotazione(senghor.getId(), p5);
         a.addUtentePrenotazione(senghor.getId(), p6);
@@ -61,15 +61,8 @@ public class Main {
         a.addAbitPrenotazione(a1, p5);
         a.addAbitPrenotazione(a2, p6);
 
-        a.get5TopUtenti();
-
-        Abitazione abitazione = a.getLastMonthMostPopularAbitazione();
-        Prenotazione provaLast = a.getLastPrenotazione(antonio.getId());
-
-        System.out.println("Media Posti Letto: " + a.mediaPostiLetto());
-
-        Prenotazione prova = a.getLastPrenotazione(pierluigi.getId());
-        Set<UtenteHost> superHosts = a.getAllSuperHosts();
+        System.out.println("\nSTAMPO TUTTI I SUPER HOST");
+        System.out.println(a.getAllSuperHosts());
 
         System.out.println("\nSTAMPO TUTTI GLI UTENTI HOST E LE LORO ABITAZIONI");
         System.out.println(a.printUtentiHostAbitazioni());
@@ -79,5 +72,25 @@ public class Main {
 
         System.out.println("\nSTAMPO TUTTE LE ABITAZIONI E LE LORO PRENOTAZIONI");
         System.out.println(a.printAbitazionePrenotazioni());
+
+        System.out.println("TOP 5 UTENTI CON PIU' GIORNI PRENOTATI ULTIMO MESE ");
+        System.out.println(a.get5TopUtenti());
+
+        System.out.println("\nSTAMPO L'ABITAZIONE CON PIU' PRENOTAZIONI NELL'ULTIMO MESE");
+        System.out.println(a.getLastMonthMostPopularAbitazione());
+
+        System.out.println("STAMPO L'UTENTE HOST CON PIU' PRENOTAZIONI NELL'ULTIMO MESE");
+        System.out.println(a.getLastMonthMostPopularHost());
+
+        System.out.println("\nULTIMA PRENOTAZIONE DI EMILIAN E UTENTE NON ESISTENTE");
+        try {
+            System.out.println(a.getLastPrenotazione(emilian.getId()));
+            System.out.println(a.getLastPrenotazione(UUID.randomUUID()));
+        }
+        catch (UtenteNotFoundException u){
+            System.out.println(u.getMessage());
+        }
+
+        System.out.println("\nMedia Posti Letto: " + a.mediaPostiLetto());
     }
 }
